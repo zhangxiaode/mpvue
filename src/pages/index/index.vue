a4cebe003957a5c02d60c373462a2f0169fdfa00
zhangxiaode <2279323443@qq.com>
2019-03-19 16:31:37 +0800

    项目初始化

---
+++ src/pages/index/index.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    zxding
+  </div>
+</template>
+
+<script>
+
+export default {
+  data () {
+    return {
+    }
+  },
+
+  components: {
+  },
+
+  methods: {
+  },
+
+  created () {
+    // let app = getApp()
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
